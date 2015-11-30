346b0277be204d7a4236fa358789b6dd0db29e0a
TibbeFeniks <tibbe.hoppener@xs4all.nl>
2015-11-30 16:22:21 +0100

    Initial commit of default create project in AndroidStudio

---
+++ app/src/main/res/NewCppClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owner on 11/23/2015.
+//
+
+#include "NewCppClass.h"
---
+++ app/src/main/res/NewCppClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Owner on 11/23/2015.
+//
+
+#ifndef MY_APPLICATION_NEWCPPCLASS_H
+#define MY_APPLICATION_NEWCPPCLASS_H
+
+
+class NewCppClass {
+
+};
+
+
+#endif //MY_APPLICATION_NEWCPPCLASS_H
